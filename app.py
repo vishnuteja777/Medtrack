3c2e3fb4b02cd368a0f3540e4cabd210a532687f
vishnuteja777 <228x1a42c9@khitguntur.ac.in>
2025-07-04 12:38:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 from functools import wraps
 
 app = Flask(__name__)
-app.secret_key = 'your-secret-key-change-in-production'
+app.secret_key = '2f9b3e7d56d749cdab6f9cf672d2a937c64f0f19c4eaefc17264f2d39a5314bb'
 
 # AWS Configuration
 AWS_REGION = 'us-east-1'
